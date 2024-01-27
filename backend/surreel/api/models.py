4155daae23fbb9337e3d83a5cc065a0af0e0d469
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-01-27 18:30:10 -0500

    Created Models

--- backend/surreel/api/models.py
+++ backend/surreel/api/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30)
+    password = models.PasswordField(max_length=30)
+    image = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name ='posts', on_delete=models.CASCADE, db_index=True)
+    caption = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Media(models.Model):
+    post = models.ForeignKey(Post, related_name='media', on_delete=models.CASCADE, db_index=True)
+    url = models.URLField()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE, db_index=True)
+
+    class Meta:
+        unique_together = ['user', 'post']
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['follower', 'followed']
+
+class Room(models.Model):
+    participants = models.ManyToManyField(User, related_name='rooms', db_index=True)
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE, db_index=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    media_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
