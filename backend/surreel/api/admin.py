e9b35fbb5789bd4bd7f6b80a7d046abf62311dcf
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-01-27 21:45:07 -0500

    Migrated Models and registered them into the Django admin console

--- backend/surreel/api/admin.py
+++ backend/surreel/api/admin.py
@@ -1,3 +1,25 @@
+
 from django.contrib import admin
+from .models import User, Post, Media, Like, Follow, Room, Message
 
 # Register your models here.
+
+@admin.register(User)
+class UserModel(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'created_at', 'updated_at')
+
+@admin.register(Post)
+class PostModel(admin.ModelAdmin):
+    list_display = ('user', 'caption', 'created_at', 'updated_at')
+
+@admin.register(Media)
+class MediaModel(admin.ModelAdmin):
+    list_display = ('post', 'url')
+
+@admin.register(Room)
+class RoomModel(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Message)
+class MessageModel(admin.ModelAdmin):
+    list_display = ('room', 'sender', 'media_url', 'created_at')
--- backend/surreel/api/models.py
+++ backend/surreel/api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     image = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
