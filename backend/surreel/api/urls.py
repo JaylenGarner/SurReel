64ba4e1bceaa811181a32099e118a747a68c905f
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-02-24 19:26:58 -0500

    Implemented route to like a post

--- backend/surreel/api/urls.py
+++ backend/surreel/api/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from .views.posts import PostList, PostDetails, UserPosts
+from .views.likes import LikeList
 
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
     path('posts/<int:id>/',  PostDetails.as_view()),
-    path('<int:user_id>/posts/', UserPosts.as_view())
+    path('posts/<int:id>/likes/', LikeList.as_view()),
+    path('<int:user_id>/posts/', UserPosts.as_view()),
 ]
--- backend/surreel/api/views/likes.py
+++ backend/surreel/api/views/likes.py
@@ -1,1 +1,21 @@
+from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from ..models import Like
 from ..serializers import LikeSerializer
+
+
+class LikeList(APIView):
+
+    def post(self, request, id):
+        request.data['post'] = id
+        print('REQUEST', request.data)
+        serializer = LikeSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
